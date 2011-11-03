32afda520d374f65626e656afba4626e21db8161
Nayuta Taga <nayuta@ganaware.jp>
2011-11-03 17:31:25 +0900

    * README changed * package.sh changed

--- package.sh
+++ package.sh
@@ -2,10 +2,19 @@
 
 PVR=`basename win-ssh-agent-*.cygport .cygport`
 
-cygport $PVR finish
-cygport $PVR download
-cygport $PVR prep
+rm -rf /usr/src/win-ssh-agent*
+rm -f $PVR.tar.bz2
+rm -f ${PVR}-src.tar.bz2
+cygport ./$PVR.cygport finish
+
+cygport ./$PVR.cygport download
+
+cygport ./$PVR.cygport prep
 cp CYGWIN-PATCHES/* $PVR/src/win-ssh-agent/CYGWIN-PATCHES/
-cygport $PVR compile
-cygport $PVR install
-cygport $PVR package
+if [ ! -d $PVR/src/win-ssh-agent/CYGWIN-PATCHES/ ]; then exit; fi
+
+cygport ./$PVR.cygport compile
+
+cygport ./$PVR.cygport install
+
+cygport ./$PVR.cygport package
